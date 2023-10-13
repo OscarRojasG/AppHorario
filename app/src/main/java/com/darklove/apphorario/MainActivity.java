45ef3ecf2a05c1b9f768c6ac4c2ef647d2936e7f
OscarRojasG <oscar.rojas.g@mail.pucv.cl>
2023-10-13 03:12:21 -0300

    basic layout for login activity

--- app/src/main/java/com/darklove/apphorario/MainActivity.java
+++ app/src/main/java/com/darklove/apphorario/MainActivity.java
@@ -3,6 +3,10 @@ package com.darklove.apphorario;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText etRut, etPassword;
+        Button btnIngresar;
+
+        etRut = findViewById(R.id.etRut);
+        etPassword = findViewById(R.id.etPassword);
+        btnIngresar = findViewById(R.id.btnIngresar);
+
+        btnIngresar.setOnClickListener(view -> {
+            String rut = etRut.getText().toString();
+            String password = etPassword.getText().toString();
+
+            Toast.makeText(getApplicationContext(), "RUT " + rut + ", Clave " + password, Toast.LENGTH_LONG).show();
+        });
+
     }
+
 }
